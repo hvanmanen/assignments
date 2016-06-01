e9c6838da9bbb32316c28006acb9a07ad03b59fb
braadworst <roy@royniels.nl>
2016-06-02 01:10:37 +0200

    added new assignments

---
+++ public/assignments/02-iterations-scope/braadworst/js/script.js
@@ -0,0 +1,41 @@
+/*
+  Create a for loop that counts down from 20 to 1
+  For each iteration log the current counter
+*/
+
+/*
+  Create a for loop that creates the following arrays
+  [17, 34, 51, 68, 85], [17, 34, 51, 68, 119] and [17, 34, 51, 68, 200]
+  Each array in a seperate loop
+*/
+
+/*
+  Create while loop, multiple 2 by 2 and continue doing that until you reach 1.000.000
+  return the amount of steps it took
+*/
+
+/*
+  Use the array map function to convert the following input array to the output array
+  input [10, 20, 30, 40, 50]
+  output [5, 10, 15, 20, 25]
+*/
+
+/*
+  Use the array filter method to remove all characters that are not from the simpsons
+  ['Smithers', 'Ernie', 'Homer', 'Bert', 'Bart', 'Police chief Wiggum', 'Baloo']
+  For the people who don't know the characters from the Simpsons, Ernie, Bert and Baloo are not in the show.
+  But more importantly go watch the show instead of doing these assignments. Ahum.
+*/
+
+/*
+  Convert the following object to an array
+  The array should look like ['klarendal', 'alteveer', ...]
+*/
+let suburbsInArnhem = {
+  klarendal : 'noord',
+  alteveer : 'noord',
+  schuitgraaf : 'zuid',
+  lombok : 'noord',
+  heijenoord : 'noord',
+  spijkerkwatier : 'noord'
+};
